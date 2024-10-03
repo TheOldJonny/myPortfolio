177c729facc194e212ce83e3ffc782f8bc7d1071
Jonathan Campeggio <62011223+TheOldJonny@users.noreply.github.com>
2024-10-03 10:56:59 +0200

    Add files via upload
    Bug fix in the singly linked list implementation

--- linked_list.py
+++ linked_list.py
@@ -1,39 +1,35 @@
-#!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
-
+# This is an implementation of the node of a singly linked list
 class SinglyLinkedListNode:
     def __init__(self, node_data):
+        """The class constructor
+
+        Args:
+            node_data (_type_): the data contained by the node
+        """
         self.data = node_data
-        self.next = None
+        # next is the next node connected to the actual node
+        self.next = None     
         
+    # method to print a node
     def __str__(self):
         return str(self.data)
     
-    # def __add__(self, other):
-    #     if not(isinstance(other)
-
+# Class that implements a singly linked list
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
         self.length = 0
 
-    @staticmethod
-    def print_singly_linked_list(node, sep):
-        while node:
-            print(str(node.data), end='')
-
-            node = node.next
-
-            if node:
-                print(sep, end='')
-
-
     def append(self, data):
+        """adding a new node at the end of the list
+
+        Args:
+            data (int, float, str): the new data to insert as the last node
+
+        Returns:
+            SinglyLinkedList: it returns the updated list 
+        """
         
         if not(isinstance(data, int)) and not(isinstance(data, str)) and not(isinstance(data, float)):
             print("ERROR: You can append only integers, strings, or floating numbers")
@@ -50,12 +46,23 @@ class SinglyLinkedList:
         return self
             
     def insertAtFront(self, data):
+        """Inserting a new node containing data at the list front
+
+        Args:
+            data (int, float, str): data to be inserted
+        """
         old_node = self.head
         self.head = SinglyLinkedListNode(data)
         self.head.next = old_node
         self.length += 1
             
     def insertAt(self, position, data):
+        """Inserting a new node at position position with the data data
+
+        Args:
+            position (int): the position where insert the data
+            data (int, float, str): _description_
+        """
         assert position < self.length, "ERROR: the insertion position is greater than the linked list length"
         
         assert position > 0, "ERROR: the insertion position cannot be a negative number"
@@ -71,20 +78,25 @@ class SinglyLinkedList:
         inserted_node.next = old_node
         previous_node.next = inserted_node
         self.length += 1
-        return
     
     def pop(self):
+        """Removing the last element of a linked list
+        """
         assert self.length >0, "ERROR: you cannot remove an element from an empty linked list"
         
         previous_node = self.head
-        for i in range(self.length - 2):
+        for _ in range(self.length - 2):
             previous_node = previous_node.next
             
         previous_node.next = None
         self.length -= 1
-        return
     
     def deleteAt(self, position):
+        """Removing the node at position
+
+        Args:
+            position (int): the position of the node to be deleted
+        """
         assert position < self.length, "ERROR: the delete position is greater than the linked list length"
         
         if self.length == 1:
@@ -108,13 +120,17 @@ class SinglyLinkedList:
         self.length -= 1
         return
     
-    # maybe it is better as a static method
     def printInReverse(self):
-        print("reversed")
+        """
+        Printing the list in reverse order
+        """
         for i in reversed(range(0, self.length - 1)):
             print(self.__getitem__(i).data)
             
     def reverse(self):
+        """
+        Reversing the list
+        """
         if self.length == 0 or self.length == 1:
             return self.head
         
@@ -134,6 +150,11 @@ class SinglyLinkedList:
         return
     
     def min(self):
+        """the minimum function
+
+        Returns:
+            SinglyLinkedListNode: the node that contains the minimum value
+        """
         if self.length == 0:
             return None
         elif self.length == 1:
@@ -146,10 +167,14 @@ class SinglyLinkedList:
                     min = self.__getitem__(i).data
                     node_min = self.__getitem__(i)
             
-            # print(min) 
             return node_min
         
     def max(self):
+        """the max function
+
+        Returns:
+             SinglyLinkedListNode: the node that contains the maximum value
+        """
         if self.length == 0:
             return None
         elif self.length == 1:
@@ -162,12 +187,17 @@ class SinglyLinkedList:
                     max = self.__getitem__(i).data
                     node_max = self.__getitem__(i)
             
-            # print(max) 
             return node_max
     
-    # Function that searches a value in the linked list and returns its position
-    # If the values is not present, it returns -1
     def find(self, value):
+        """Function that searches a value in the linked list and returns its position
+
+        Args:
+            value (int, float, str): the value to search for
+
+        Returns:
+            int: the value index. If the values is not present, it returns -1
+        """
         index = -1
         if self.length == -1:
             return index
@@ -177,23 +207,34 @@ class SinglyLinkedList:
                 index = i
                 break
         return index
-                    
+                   
+    # method to copy a list 
+    def copy(self):
+        copied = SinglyLinkedList()
+        for i in range(self.length):
+            copied.append(self.__getitem__(i).data)
+            
+        return copied
+    
     def sort(self):
+        """Sorting the list in ascending order
+        """
         sorted = SinglyLinkedList()
-        while self.length != 0:
-            minimum = self.min().data
-            index = self.find(minimum)
+        copied = self.copy()
+        while copied.length != 0:
+            minimum = copied.min().data
+            index = copied.find(minimum)
             sorted.append(minimum)
-            self.deleteAt(index)
-            print("The minimum", minimum)
-            print("The index", index)
-            print("The unsorted list", self)
-            print("The sorted list", sorted)
+            copied.deleteAt(index)
             
         return sorted
     
-    # function that returns a linked list withoud duplicated values
     def unique(self):
+        """Unique values
+
+        Returns:
+            SinglyLinkedList: list that contains the unique values from the linked list
+        """
         if self.length == 0 or self.length == 1:
             return self.head
         
@@ -202,24 +243,33 @@ class SinglyLinkedList:
         value = self.head.data
         myList.append(value)
         actual_node = self.head
-
+        
+        copied = self.copy()
         while True:
             # loop that removes all the occurrence of value
-            while self.find(value) != -1:
-                self.deleteAt(self.find(value))    
+            while copied.find(value) != -1:
+                copied.deleteAt(copied.find(value))
             
-            
-            if self.length ==0:
+           
+            if copied.length ==0:
                 return myList
             
-            actual_node=self.head
+            actual_node=copied.head
             value = actual_node.data
             myList.append(value)
             
             
         
     
-    def __getitem__(self, index):
+    def __getitem__(self, index: int):
+        """
+        Accessing self[index]
+        Args:
+            index (int): index of the selected node inside the list
+
+        Returns:
+            SinglyLinkedListNode: selected node of the list
+        """
         assert index < self.length, "ERROR: index out of range"
         
         node = self.head
@@ -230,9 +280,19 @@ class SinglyLinkedList:
             
     
     def __len__(self):
+        """Implementing the len method
+
+        Returns:
+            int: length of the linked list
+        """
         return self.length
     
     def __str__(self):
+        """Implementing the print(self) method
+
+        Returns:
+            str: a human-readable string that represents the list
+        """
         my_string='['
         for i in range(self.length):
             my_string+= str(self.__getitem__(i).data)
@@ -244,6 +304,14 @@ class SinglyLinkedList:
     
     # overloading the + method
     def __add__(self, other):
+        """_summary_
+
+        Args:
+            other (SinglyLinkedList): the other linked list to be added to self
+
+        Returns:
+            SinglyLinkedList: the sum list
+        """
         if isinstance(other, SinglyLinkedList):
             last_node = self.__getitem__(self.length - 1)
             last_node.next = other.head
@@ -255,110 +323,50 @@ class SinglyLinkedList:
             print("ERROR: you can add only an other single linked list, a float, an integer or a string")
             exit(-1)
         
-    
-# function that returns the length of a li     
-def length(llist):
-    l = 0
-    
-    node = llist
-    while node:
-        l += 1
-        node = node.next
-    
-    return l
-
-# function that returns the selected node
-def selectNode(llist, index):
-    l = length(llist)
-    assert index < l, "ERROR: the deletion index is greater than the linked list dimension"
-    
-    node = llist
-    for i in range(index):
-        node = node.next
-        
-    return node
-
-    
-            
+                
 if __name__ == '__main__':
      
-
+    # 1. Instance and appending some values
     llist = SinglyLinkedList()
     
-    llist_count = 10
+    llist_count = 3
 
     for i in range(llist_count):
         llist_item = 3
         llist.append(llist_item)
         
-    llist.append(4)
-    llist.append(4)
-    llist.append(4)
+    print(llist)
     
+    
+    # 2. Appending some values and finding the unique values
+    
+    llist.append(0)
     llist.append(5)
-    
+    llist.append(8)
     llist.append(0)
-    llist.append(35)
-    llist.append(10)
-    llist.append(0)
-    llist.append(7)
-    llist.append(35)
+    llist.append(5)
+    llist.append(5)
     
     
     print(llist)
     
     
-    a = llist.unique()
-    print(a)
-
-    # llist.insertAtFront(30)
-    # llist.insertAtFront(40)
-    # llist.insertAtFront(10)
-    # print(llist)
     
-    # print(llist.max())
-    # a = llist.sort()
+    unique_values = llist.unique()
+    print(unique_values)
+    
+
+    # 3. Inserting some data and finding the max
+    llist.insertAtFront(30)
+    llist.insertAtFront(40)
+    llist.insertAtFront(10)
+    print(llist)
+    
+    # 4. Printing the max of the linked list
+    print(llist.max())
+    
+    # 5. Sorting the list
+    sorted = llist.sort()
+    print(sorted)
         
-    # print(llist.find(1000))
-    # a = llist.sort()
-    # llist.deleteAt(len(llist) - 1)
-    # print(llist)
-    # llist = llist + 5
-    # print(llist)
-    # llist.min()
-    
-    # SinglyLinkedList.print_singly_linked_list(llist.head, '\n')
-    
-    # print('\n')
-    
-    # print(llist)
-    
-    
-    # # llist.printInReverse()
-    # llist.reverse()
-    
-    
-    # SinglyLinkedList.print_singly_linked_list(llist.head, '\n')
-    # print("\n")
-    # print(len(llist))
-    
-    # print("\n\n")
-    
-    # llist.deleteAt(3)
-    # SinglyLinkedList.print_singly_linked_list(llist.head, '\n')
-    # print("\n")
-    # print(len(llist))
 
-    # print(length(llist.head))
-    
-    # print(selectNode(llist.head, 2).data)
-    
-    # l2 = SinglyLinkedList()
-    # l2.append(5)
-    # l2.append(6)
-    # l2.append(100)
-    
-    
-    
-    # l3 = llist + l2
-    # print(l3)
